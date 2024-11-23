31bf37a3de1e49f900563130560d25577abef613
MJServices <mjservices410@gmail.com>
2024-11-23 12:46:10 +0500

    Navbar Component + some more

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -1,15 +1,15 @@
+import BgVideo from "./BgVideo";
+import Navbar from "./Navbar";
+
 const LandingPage = () => {
   return (
     <section className="w-screen h-screen">
-         <video autoPlay muted loop className="w-[100vw] h-[90vh] object-cover">
-        <source src="/videos/GOJO VS SUKUNA FULL FIGHT AMV + ENDING SCENE 4K ｜ LADY GAGA - JUDAS.mp4" type="video/mp4" />
-        Your browser does not support the video tag.
-      </video>
-        <nav>
-
-        </nav>
-        <section></section>
+      <BgVideo />
+      <section className="relative z-[2] w-screen h-screen">
+        <Navbar/>
+        
+      </section>
     </section>
-  )
-}
-export default LandingPage
+  );
+};
+export default LandingPage;
